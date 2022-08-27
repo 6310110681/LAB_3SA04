29fec1566dcf9338729eb14f3316a10e2908281a
Onint <ekcept2467@gmail.com>
2022-08-27 23:58:03 +0700

    PART 6 ปรับหน้า Weather

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,42 +1,42 @@
-import React from "react"
-import {View, Text, StyleSheet} from 'react-native'
-
-export default function Forecast(props){
-    return(
-        <View>
-            <Text style={style.Text}>main</Text>
+import React from "react";
+import { View, Text , StyleSheet } from "react-native";
+  export default function Forecast(props) {
+      return (
+          <View> 
+         
+              <View style= {{
+                  flexDirection: "row",
+                  justifyContent: 'center',
+              }}>
+                <Text style={style.temp}>{props.temp}</Text>
+                     <Text></Text>
+                  <Text style={style.Text}>  °C</Text>
+                  
+            </View>
+            <View>
+            <Text style={style.Text}></Text>
+            </View>
             <Text style={style.Text}>{props.main}</Text>
-             <Text style={style.Text}>description</Text>
-             <Text style={style.Text}>{props.description}</Text>
-             <View style= {{
+            <Text style={style.Text}>------------------------------------</Text>
+            <View style= {{
                  flexDirection: "row",
-                 justifyContent: 'center',
-
+                 justifyContent: 'space-between',
              }}>
-                 <Text style={style.temp}>{props.temp}</Text>
-                    <Text></Text>
-                 <Text style={style.Text}>  °C</Text>
+                 <View>
+                     <Text style={style.Text}>   Humidity    {props.humidity} %  </Text>
+                     <Text> </Text>
+                     <Text style={style.Text}>   Feel Like    {props.feels_like} °C    </Text>
                  </View> 
-        </View>
-    )
+             <View>
+                     <Text> </Text>
+                     <Text> </Text>
+                     <Text style={style.Text}>{props.description}</Text>
+            </View>
+             <Text> </Text>
+             </View>
+             <Text style={style.Text}>~~~~~~~~~~~~~~~~~~~~~~~~~~</Text>
+
+
+          </View> 
+      );
 }
-
-        const style = StyleSheet.create(
-        {
-            Text: {
-                textAlign: 'center',
-                fontSize: 30,
-                fontWeight: "bold",
-                color: 'white',
-
-            },
-
-            temp: {
-                fontSize: 40, 
-                fontWeight: "bold", 
-                color: 'white', 
-                textAlign: 'center',
-                lineHeight: 40
-            }
-    }
-)  
--- components/Weather.js
+++ components/Weather.js
@@ -3,65 +3,69 @@ import { Text, ImageBackground, StyleSheet, View } from "react-native";
 import Forecast from "./Forecast";
 import Constants from 'expo-constants';
 
- export default function Weather(props) {
-     const [forecastInfo, setForecastInfo] = useState(
-         {            
-            main: '-',
-            description: '-',
-            temp: 0
-        }
-    )
-
-    useEffect(() => {
-        console.log(`fetching data with zipCode = ${props.zipCode}`);
-        if (props.zipCode) {
-          fetch(
-            `http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=e755c7959fb15b66f0a808112a1c7537`
-          )
-            .then((response) => response.json())
-            .then((json) => {
-              setForecastInfo({
-                main: json.weather[0].main,
-                description: json.weather[0].description,
-                temp: json.main.temp,
-              });
-            })
-            .catch((error) => {
-              console.warn(error);
-            });
-        }
-      }, [props.zipCode]);
-
-
-    return (
-        <ImageBackground source={require('../bg_sky.jpg')} style={style.backdrop}>
-               <View style={style.highlight}>
-                 <Text style={style.titleText}>Zip code is {props.zipCode}.</Text>
-                 <Forecast {...forecastInfo}/>
-             </View>
-         </ImageBackground>
-
-     );
- }
- const style = StyleSheet.create(
-     {
-         backdrop: {
-             alignItems: 'center',
-             width: '100%',
-             height: '100%'
-         },
-         highlight: {
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            width:"100%", 
-            height:"35%", 
-            paddingTop: Constants.statusBarHeight, 
-            alignItems: 'center'
-         },
-        titleText: {
-            fontSize: 25,
-            fontWeight: "bold",
-            color: 'white',
-            textAlign: 'center'
-         }
+export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: 'loading..',
+          description: 'loading',
+          temp: 0,
+          humidity: 'loading',
+          feels_like: 'loading',
+          
+      }
+  )
+  useEffect(() => {
+    console.log(`fetching data with zipCode = ${props.zipCode}`);
+    if (props.zipCode) {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=e755c7959fb15b66f0a808112a1c7537`
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          setForecastInfo({
+            main: json.weather[0].main,
+            description: json.weather[0].description,
+            humidity: json.main.humidity,
+            temp: json.main.temp,
+            feels_like: json.main.feels_like,
+          });
+        })
+        .catch((error) => {
+          console.warn(error);
+        });
     }
+  }, [props.zipCode]);
+  return (
+
+    <ImageBackground source={require('../bg_sky.jpg')} style={style.backdrop}>
+           <View style={style.highlight}>
+             <Text style={style.titleText}>Zip code is {props.zipCode}.</Text>
+             <Text> </Text>
+             <Forecast {...forecastInfo}/>
+         </View>
+     </ImageBackground>
+ );
+}
+
+const style = StyleSheet.create(
+{
+  backdrop: {
+       alignItems: 'center',
+       width: '100%',
+       height: '100%'
+      },
+      highlight: {
+         backgroundColor: 'rgba(0, 0, 0, 0.5)',
+         width:"100%", 
+         height:"45%", 
+         paddingTop: Constants.statusBarHeight, 
+         alignItems: 'center'
+         
+     },
+     titleText: {
+         fontSize: 25,
+         fontWeight: "bold",
+         color: 'white',
+         textAlign: 'center'
+     }
+ }
 ) 
